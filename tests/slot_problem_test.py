fa40131b30e8dadaadf24ed023cd0089f450a48e
Saurabh Machave <tkshnwesper@users.noreply.github.com>
2025-08-22 21:15:03 +0530

    add illustration test cases

--- tests/slot_problem_test.py
+++ tests/slot_problem_test.py
@@ -16,7 +16,35 @@ def test_time_slot_not_available(now):
                      ])
 
 
+### below test cases are not necessary - for illustration purpose only ###
+
+
 def test_time_slot_available_when_no_overlap(now):
+    assert is_time_slot_available(
+        TimeSlot(now + timedelta(minutes=30), now + timedelta(minutes=60)), [
+            TimeSlot(now, now + timedelta(minutes=30))
+        ]
+    )
+
+
+def test_time_slot_not_available_when_between_two_slots(now):
+    assert not is_time_slot_available(
+        TimeSlot(now + timedelta(minutes=15), now + timedelta(minutes=45)), [
+            TimeSlot(now, now + timedelta(minutes=30)),
+            TimeSlot(now + timedelta(minutes=40), now + timedelta(minutes=60))
+        ]
+    )
+
+
+def test_time_slot_available_when_exactly_at_start(now):
+    assert is_time_slot_available(
+        TimeSlot(now, now + timedelta(minutes=30)), [
+            TimeSlot(now + timedelta(minutes=30), now + timedelta(minutes=60))
+        ]
+    )
+
+
+def test_time_slot_available_when_exactly_at_end(now):
     assert is_time_slot_available(
         TimeSlot(now + timedelta(minutes=30), now + timedelta(minutes=60)), [
             TimeSlot(now, now + timedelta(minutes=30))
@@ -28,6 +56,6 @@ def test_time_slot_available_when_multiple_slots(now):
     assert is_time_slot_available(
         TimeSlot(now + timedelta(minutes=30), now + timedelta(minutes=60)), [
             TimeSlot(now, now + timedelta(minutes=30)),
-            TimeSlot(now + timedelta(minutes=15), now + timedelta(minutes=45))
+            TimeSlot(now + timedelta(minutes=60), now + timedelta(minutes=80))
         ]
     )
